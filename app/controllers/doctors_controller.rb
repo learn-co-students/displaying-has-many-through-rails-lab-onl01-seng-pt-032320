4fac64687851d68ae2df1cce41f9e267fc77eb9e
Vlad <VladDaImpaler@gmail.com>
2020-08-21 14:50:35 -0400

    Done.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,8 +1,42 @@
 class DoctorsController < ApplicationController
+    before_action :set_doctor, only: [:show, :edit, :update, :destroy]
+    def index
 
+    end
+    
     def show
         #Displays the doctor's name, department,
         ## and appointments, with each appointment's date, time,
         ### and patient name (linking to the patient's show page)
     end
+
+    def new
+        @doctor = Doctor.new
+    end
+
+    def create
+
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+
+    private
+
+    def set_doctor
+        @doctor=Doctor.find(params[:id])
+    end
+
+    def doctor_params(*args)
+        params.require(:doctor).permit(*args)
+    end
+
 end
