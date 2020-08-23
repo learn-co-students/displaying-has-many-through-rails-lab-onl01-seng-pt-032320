75441afd2d79efa4388b4c0dedc64190cb68867c
Vlad <VladDaImpaler@gmail.com>
2020-08-22 23:42:19 -0400

    Done.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,12 +1,39 @@
 class PatientsController < ApplicationController
-
+    before_action :set_patient, only: [:show, :edit, :destroy]
     def index
-        # Displays a link to each patient's show page
-        ## and the total number of appointments they have
+        @patients = Patient.all
     end
     
     def show
         # Lists the date and time for each of their appointments
         ## and links to the corresponding doctor's show page
+    end
+
+    def create
+    end
+    
+    def new
+    end
+    
+    def edit
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
+    private
+
+    def set_patient
+        @patient = Patient.find(params[:id])
+    end
+    
+    def patient_params(*args)
+        params.require(:patient).permit(*args)
     end
 end
